1e6b864cb1e41332d593f9d714ee25e5f3a5bb10
Dorian Martinez <dorianmzamora@gmail.com>
2024-07-15 23:28:54 -0400

    html-for-the-navbar

--- index.php
+++ index.php
@@ -10,9 +10,23 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
 </head>
 <body>
-    <h1>Hello world!</h1>
+    <!-- <h1>Hello world!</h1>
     <h3>This is my first php document</h3>
-    <p>This is a testing paragraph</p>
+    <p>This is a testing paragraph</p> -->
+
+    <header>
+        <div class="header_body">
+            <a href="./index.php" class="logo">TechnoBlingBling</a>
+            <nav class="navbar">
+                <a href="#">Add Products</a>
+                <a href="#">View Products</a>
+                <a href="#">Shopit</a>
+            </nav>
+
+            <a href="" class="icon"><i class="fa-solid fa-cart-shopping"></i></a>
+        </div>
+    </header>
+
     <!-- js file here -->
     <script src="./js/script.js"></script>
 </body>
