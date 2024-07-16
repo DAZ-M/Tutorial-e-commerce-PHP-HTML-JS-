b97648e2f76dd70beeefc42d609978d1d435a80c
Dorian Martinez <dorianmzamora@gmail.com>
2024-07-16 16:36:02 -0400

    external-php-file-for-header-section

---
+++ header.php
@@ -0,0 +1,13 @@
+<header class="header">
+        <div class="header_body">
+            <a href="./index.php" class="logo">TechnoBlingBling</a>
+            <nav class="navbar">
+                <a href="#">Add Products</a>
+                <a href="#">View Products</a>
+                <a href="#">Shopit</a>
+            </nav>
+
+            <a href="" class="icon"><i class="fa-solid fa-cart-shopping"><span><sup>4</sup></span></i></a>
+            <!-- <div id="menu-btn" class="fas fa-bars"></div> -->
+        </div>    
+</header>
--- index.php
+++ index.php
@@ -10,24 +10,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
 </head>
 <body>
-    <!-- <h1>Hello world!</h1>
-    <h3>This is my first php document</h3>
-    <p>This is a testing paragraph</p> -->
-
-    <header class="header">
-        <div class="header_body">
-            <a href="./index.php" class="logo">TechnoBlingBling</a>
-            <nav class="navbar">
-                <a href="#">Add Products</a>
-                <a href="#">View Products</a>
-                <a href="#">Shopit</a>
-            </nav>
-
-            <a href="" class="icon"><i class="fa-solid fa-cart-shopping"><span><sup>4</sup></span></i></a>
-            <!-- <div id="menu-btn" class="fas fa-bars"></div> -->
-        </div>
+    <!-- Header section -->
+    <?php include('header.php') ?>
     
-    </header>
     
     <!-- Form section -->
     <div class="container">
